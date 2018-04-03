1e3f27b682180715c932b8c4aa24e6e81f3e3771
BlackPearlCollective <info@blackpearlcollective.com>
2018-04-03 10:58:24 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var myName = "Johanne"
+var myFavColor = "Blue"
+var MyAge = "None Ya"
+
+myFunc
+
+
+function myFunc(){
+  var myFuncVar = "Hey"
+  console.log(MyAge)
+}
+
+// alert("Your computer has been infected by a virus. Please send bitcon.");
